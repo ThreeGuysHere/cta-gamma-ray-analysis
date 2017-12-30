1a001427b48a77ad3cb3f5261769a112febcbaf1
gabrielecorni <gabriele.corni@gmail.com>
2017-12-30 10:32:27 +0100

    create training set xml

--- filters/utils.py
+++ filters/utils.py
@@ -5,37 +5,64 @@ import cv2
 #  screen info
 screen_width = 1650
 screen_height = 1050
-dx = int(screen_width/3)
-dy = int(screen_height/2)
+dx = int(screen_width / 3)
+dy = int(screen_height / 2)
 label_bar_height = 65
 
 
 def img_prepare(src):
-    cv2.normalize(src, src, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
-    return src.astype(np.uint8)
+	cv2.normalize(src, src, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+	return src.astype(np.uint8)
 
 
 def get_data(src):
-    data = fits.getdata(src)
-    img = data.astype(np.uint8)
-    return img_prepare(img)
+	data = fits.getdata(src)
+	img = data.astype(np.uint8)
+	return img_prepare(img)
 
 
 def show(**kwargs):
-    x = 0
-    y = 0
-    for key in kwargs:
-        label = key
-        img = kwargs[key]
-        cv2.namedWindow(label, cv2.WINDOW_NORMAL)
-        cv2.imshow(label, img)
-        cv2.resizeWindow(label, dx, dy-label_bar_height)
-        cv2.moveWindow(label, x, y)
+	x = 0
+	y = 0
+	for key in kwargs:
+		label = key
+		img = kwargs[key]
+		cv2.namedWindow(label, cv2.WINDOW_NORMAL)
+		cv2.imshow(label, img)
+		cv2.resizeWindow(label, dx, dy - label_bar_height)
+		cv2.moveWindow(label, x, y)
 
-        screen_end = int(x / (screen_width-dx)) > 0
+		screen_end = int(x / (screen_width - dx)) > 0
 
-        x = x + dx if not screen_end else 0
-        y += dy if screen_end else 0
+		x = x + dx if not screen_end else 0
+		y += dy if screen_end else 0
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+
+def create_xml(model, output, pnt_type="equatorial", ra=83.64, dec=22.02, obsid=1, start=0.0, duration=100.0,
+				emin=0.1, emax=100, caldb="prod2", fov=10, irf="South_0.5h"):
+	with open(model, 'r') as model_xml:
+		with open(output, 'w') as output_file:
+
+			# read model
+			parametrized = model_xml.read()
+
+			# replace params
+			parametrized = parametrized.replace("PNT", str(pnt_type))
+			parametrized = parametrized.replace("RAX", str(ra))
+			parametrized = parametrized.replace("DEC", str(dec))
+			parametrized = parametrized.replace("OBS", str(obsid))
+			parametrized = parametrized.replace("STR", str(start))
+			parametrized = parametrized.replace("DUR", str(duration))
+			parametrized = parametrized.replace("EMI", str(emin))
+			parametrized = parametrized.replace("EMA", str(emax))
+			parametrized = parametrized.replace("CAL", str(caldb))
+			parametrized = parametrized.replace("FOV", str(fov))
+			parametrized = parametrized.replace("IRF", str(irf))
+
+			# write replaced
+			output_file.write(parametrized)
+
+	return parametrized
--- test/testkern.py
+++ test/testkern.py
@@ -2,10 +2,15 @@ import cv2
 from filters import utils, kernelize as k
 import numpy as np
 
+
+print("test script")
+new = utils.create_xml('../data/model.xml', '../data/ciao.xml', ra=83.0, dec=22.0)
+print(new)
+
 img = cv2.imread('../data/castello3.JPG', cv2.IMREAD_GRAYSCALE)
 out = k.gaussian_mask(img)
 
 res = np.multiply(img, out)
 
-utils.show(originale = img, kernel=utils.img_prepare(out), modificato=utils.img_prepare(res))
+utils.show(originale=img, kernel=utils.img_prepare(out), modificato=utils.img_prepare(res))
 
