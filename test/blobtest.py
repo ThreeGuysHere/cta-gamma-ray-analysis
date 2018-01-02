cc40004cf60477dca3f90c91041ed7fe69226f04
gabrielecorni <gabriele.corni@gmail.com>
2018-01-02 17:17:59 +0100

    histograms

--- test/blobtest.py
+++ test/blobtest.py
@@ -1,12 +1,19 @@
 import cv2
 from filters import utils, kernelize as k
+from matplotlib import pyplot as plt
 
 img = utils.get_data('../data/3s.fits')
 
-output = k.gaussian_median(img, 3, 15, 3)
+output = k.gaussian_median(img, 3, 7, 1)
 
-img = cv2.applyColorMap(img, cv2.COLORMAP_JET)
-output = cv2.applyColorMap(output, cv2.COLORMAP_JET)
+plt.hist(img.ravel(), 256, [0, 256])
+plt.show()
+
+plt.hist(output.ravel(), 256, [0, 256])
+plt.show()
+
+#img = cv2.applyColorMap(img, cv2.COLORMAP_JET)
+#output = cv2.applyColorMap(output, cv2.COLORMAP_JET)
 
 # output = cv2.threshold(output,127,255,cv2.THRESH_BINARY)[1]
 
