c786d412d7fef8bca7964f47ea8487901cee073f
Alessandro <alessandro.berlati@gmail.com>
2018-01-02 22:58:30 +0100

    fix

--- test/adaptest.py
+++ test/adaptest.py
@@ -6,7 +6,7 @@ import astropy.wcs as a
 filepath = '../data/3s.fits'
 img = utils.get_data(filepath)
 
-output = img.copy()
+smoothed = k.gaussian_median(img, 3, 7, 1)
 
 ksize = 13
 mean = -10
@@ -17,20 +17,20 @@ while True:
 		break
 	elif key == 82:  # up
 		ksize += 2
-	elif key == 84 and ksize>=5:  # down
+	elif key == 84 and ksize >= 5:  # down
 		ksize -= 2
 	elif key == 83:  # right
 		mean += 1
 	elif key == 81:  # left
 		mean -= 1
 	elif key == 103: # G
-		output = k.gaussian_median(output, 3, 7, 1)
+		smoothed = k.gaussian_median(smoothed, 3, 7, 1)
 	else:
 		print(key)
 
-	output = cv2.adaptiveThreshold(output, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, ksize, mean)
+	mask = cv2.adaptiveThreshold(smoothed, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, ksize, mean)
 
-	mask = cv2.dilate(output, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)), iterations=1)
+	mask = cv2.dilate(mask, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)), iterations=1)
 	# noise removal
 	# kernel = np.ones((3, 3), np.uint8)
 	# opening = cv2.morphologyEx(output, cv2.MORPH_OPEN, kernel, iterations = 1)
@@ -40,8 +40,8 @@ while True:
 	params = cv2.SimpleBlobDetector_Params()
 
 	# Change thresholds
-	params.minThreshold = 0;
-	params.maxThreshold = 256;
+	params.minThreshold = 0
+	params.maxThreshold = 256
 
 	# Filter by Area.
 	params.filterByArea = True
@@ -65,7 +65,7 @@ while True:
 	reversemask = 255 - mask
 	keypoints = detector.detect(reversemask)
 
-	im_with_keypoints = cv2.drawKeypoints(output, keypoints, np.array([]), (0, 255, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+	im_with_keypoints = cv2.drawKeypoints(mask, keypoints, np.array([]), (0, 255, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 	wcs = a.WCS(filepath)
 
@@ -77,4 +77,4 @@ while True:
 		print("RA,DEC: ({0},{1})".format(ra, dec))
 	print('======================================ksize, mean =' + str([ksize, mean]))
 
-	utils.show2(Original=img, Dilated=mask, Blobbed=im_with_keypoints)
+	utils.show2(Original=img, Smoothed=smoothed, Dilated=mask, Blobbed=im_with_keypoints)
