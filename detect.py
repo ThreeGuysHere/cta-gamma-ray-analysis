89fb3fb06ca5316a460493a037d5d3f4afb097ff
Alessandro Berlati <alessandro.berlati@gmail.com>
2018-01-15 11:36:56 +0100

    PEP8

--- detect.py
+++ detect.py
@@ -3,15 +3,14 @@ import cv2
 import argparse
 
 parser = argparse.ArgumentParser(description='Detect Sources')
-parser.add_argument('filepath', type=str,
-                    help='Path of the fits file')
-parser.add_argument('--config', type=str,
-                    help='Path of config file (default = data/default.conf)')
+parser.add_argument('filepath', type=str, help='Path of the fits file')
+parser.add_argument('--config', type=str, help='Path of config file (default = data/default.conf)')
 
 args = parser.parse_args()
 
 ext = Extractor.Extractor(args.filepath, "./", debug_prints=True, prints=True)
-if(args.config):
+
+if args.config:
 	ext.load_config(args.config)
 
 ext.perform_extraction()
